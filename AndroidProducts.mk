2a0841cb51888eba66d4eb73a22af4498a18a822
dupazlasu <dupazlasu66@gmail.com>
2025-08-03 00:54:17 +0200

    treewide: unify s5e9925 models
    Co-authored-by: Harder258 <jorge.erc007@gmail.com>
    Co-authored-by: Gabriel2392 <gabriel824m@gmail.com>
    Co-authored-by: KrutosVIP <52695559+KrutosVIP@users.noreply.github.com>
    Co-authored-by: ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>
    Signed-off-by: dupazlasu <dupazlasu66@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,7 +6,11 @@
 #
 
 PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_r0s.mk \
+    $(LOCAL_DIR)/twrp_g0s.mk \
     $(LOCAL_DIR)/twrp_b0s.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_b0s-eng
+    twrp_r0s-eng \
+    twrp_g0s-eng \
+    twrp_b0s-eng    
